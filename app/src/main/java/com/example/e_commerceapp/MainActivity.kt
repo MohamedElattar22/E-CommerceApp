5b25944ae03f83dd90cdfc5f71655a86a9bd109b
Killua <108879810+MohamedElattar22@users.noreply.github.com>
2023-10-28 12:40:34 +0200

    Colors And fonts Added

---
+++ app/src/main/java/com/example/e_commerceapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.e_commerceapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
